9d0eb71bc3e886046a97d3fb2c8028aa0402e4ba
RobertoJustino <robertojustino@live.fr>
2022-09-29 06:31:41 -0400

    crud_ajout

--- app/app.py
+++ app/app.py
@@ -1,11 +1,16 @@
 from typing import List, Dict
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request, flash, redirect
 import mysql.connector
 import json
 
 app=Flask(__name__,template_folder='templates')
 
-@app.route('/mangas')
+app.secret_key = 'secret key'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 def mangas():
     config = {
         'user': 'root',
@@ -23,15 +28,47 @@ def mangas():
 
     return results
 
+@app.route('/mangas')
+def listMangas():
+    return render_template('mangas.html', data=mangas())
 
-""" @app.route('/')
-def index():
-   return json.dumps({'favorite_colors': favorite_colors()}) """
+		
+@app.route('/manga/create', methods=['POST'])
+def add_manga():
+    _title = request.form['inputTitle']
+    _image = request.form['inputImage']
+    _auteur = request.form['inputAuteur']
+    _description = request.form['inputDescription']
+    _year = request.form['inputYear']
+	# validate the received values
+    if _title and _image and _auteur and _description and _year and request.method == 'POST':
+		# save edits
+        config = {
+        'user': 'root',
+        'password': 'root',
+        'host': 'db',
+        'port': '3306',
+        'database': 'goodreads'
+        }
+        conn = mysql.connector.connect(**config)
+        cursor = conn.cursor()
+        sql = "INSERT INTO mangas(title, img_src, author, description, year) VALUES(%s, %s, %s, %s, %s)"
+        data = (_title, _image, _auteur, _description, _year)
+        cursor.execute(sql, data)
+        conn.commit()
+        flash('User added successfully!')
+        cursor.close()
+        conn.close()
+        return redirect('/')
+    else:
+        cursor.close()
+        conn.close()
+        return 'Error while adding user'
+    
 
-@app.route('/')
-def index():
-    return render_template('index.html', data=mangas())
-    #    return render_template('index.html', data=json.dumps({'mangas': mangas()}))
+@app.route('/add_manga')
+def add_manga_form():
+	return render_template('manga_form.html')
 
 
 
